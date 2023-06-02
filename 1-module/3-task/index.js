68b7b563bd6c0c7d0c33164b69c2386cd89fdadc
Your Name <you@example.com>
2023-06-02 10:58:19 +0300

    done

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,9 @@
 function ucFirst(str) {
-  // ваш код...
+  if(str.length > 1) {
+	  return str.charAt(0).toUpperCase() + str.slice(1);
+  } else if(str.length === 1) {
+	  return str.toUpperCase();  
+  } else {
+	  return "";
+  } 
 }
